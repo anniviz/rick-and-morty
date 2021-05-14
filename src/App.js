0789fb03c1f1644757ec7cbab77b74f5b2e89e95
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-14 13:47:40 +0200

    Add Nav to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import './_variables.css'
 import Card from './Card'
 import Header from './Header'
+import Nav from './Nav'
 
 export default function App() {
   const [characters, setCharacters] = useState([])
@@ -15,16 +17,19 @@ export default function App() {
 
   return (
     <div className="App">
-      <Header></Header>
-      {characters.map(({ id, name, image, species, status }) => (
-        <Card
-          key={id}
-          name={name}
-          image={image}
-          species={species}
-          status={status}
-        ></Card>
-      ))}
+      <Header />
+      <div className="App__card-container">
+        {characters.map(({ id, name, image, species, status }) => (
+          <Card
+            key={id}
+            name={name}
+            image={image}
+            species={species}
+            status={status}
+          ></Card>
+        ))}
+      </div>
+      <Nav />
     </div>
   )
 }
